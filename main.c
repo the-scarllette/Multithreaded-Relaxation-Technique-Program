c3722050cbfac1c520f1056800939dca5823cd62
Scarllette Ellis <re412@bath.ac.uk>
2021-10-29 15:52:23 +0100

    Added function to iterate values in the 2d array and copy the boundary of 2 arrays

--- main.c
+++ main.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
 
+int get_array_index(int i, int j, int size){
+    return size*i + j;
+}
+
+double average_value(double *array, int index, int size){
+    return (array[index - 1] + array[index + 1] + array[index - size] + array[index + size])/4.0;
+}
+
+void average_square(double *array, double *new_array, int size){
+    int index;
+    for(int i = 1; i < size - 1; i++){
+        for(int j = 1;j < size - 1; j++){
+            index = get_array_index(i, j, size);
+            new_array[index] = average_value(array, index, size);
+        }
+    }
+}
+
+void copy_boundary(double *array, double *new_array, int size){
+    //copying top edge
+    for(int k = 0; k < size; k++){
+        new_array[k] = array[k];
+    }
+
+    //copying bottom edge
+    int index;
+    for(int k = 0; k < size; k++){
+        index = size*(size - 1) + k;
+        new_array[index] = array[index];
+    }
+
+    //copying right edge
+    for(int k = 0; k < size; k++){
+        index = size*k;
+        new_array[index] = array[index];
+    }
+
+    //copying left edge
+    for(int k = 0; k < size; k++){
+        index = (k + 1)*size - 1;
+        new_array[index] = array[index];
+    }
+}
+
 void print_square(double *to_print, int size){
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
--- test.c
+++ test.c
@@ -1,23 +1,38 @@
 #include <malloc.h>
 #include <time.h>
+#include <stdio.h>
 
 void print_square(double *to_test, int size);
+double average_value(double *array, int index, int size);
+void average_square(double *array, double *new_array, int size);
+void copy_boundary(double *array, double *new_array, int size);
 
 int main(){
     srand(time(NULL));
-    int size = 3;
+    int size = 4;
     int k = 0;
-    
 
     double *to_test = malloc(size*size*sizeof(double));
-    for(int i = 0; i <= size; i++){
+    double *new_array = malloc(size*size*sizeof(double));
+
+    for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
-            to_test[size*i + j] = ((double)rand())/((double)rand());
-            k++;
+            to_test[size*i + j] = (double)(rand() % 10);
         }
     }
 
+    printf("Starting Array:\n");
     print_square(to_test, size);
+    printf("\n");
+
+    copy_boundary(to_test, new_array, size);
+    average_square(to_test, new_array, size);
+
+    printf("Iterated Array\n");
+    print_square(new_array, size);
+    printf("\n");
+
     free(to_test);
+    free(new_array);
     return 0;
 };
