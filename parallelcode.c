bb8eb9a7f719edbda914dade02d5c4164d51a8bd
Scarllette Ellis <re412@bath.ac.uk>
2021-11-07 22:50:06 +0000

    added barriers to stop need of creating and destroying threads

--- parallelcode.c
+++ parallelcode.c
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <pthread.h>
 
-pthread_barrier_t barrier1, barrier2;
+pthread_barrier_t barrier;
 
 struct doublesArraySection{
     int size;
@@ -14,7 +14,8 @@ struct doublesArraySection{
     int row_start;
     int num_rows;
     bool keep_iterating;
-    pthread_mutex_t lock;
+    pthread_mutex_t lock1;
+    pthread_mutex_t lock2;
 } doublesArraySection;
 
 int rand(void);
@@ -32,7 +33,6 @@ void* average_section(void *sectionaddress){
     struct doublesArraySection *arraySection = sectionaddress;
     int index;
     while((*arraySection).keep_iterating) {
-        printf("Thread on row %d start iterating\n", (*arraySection).row_start);
         //iterating array1 and storing result in array2
         for (int i = (*arraySection).row_start; i < (*arraySection).row_start + (*arraySection).num_rows; i++) {
             for (int j = 1; j < (*arraySection).size; j++) {
@@ -40,20 +40,21 @@ void* average_section(void *sectionaddress){
                 (*arraySection).array2[index] = average_value((*arraySection).array1, index, (*arraySection).size);
             }
         }
-        printf("Thread on row %d iterated now waiting\n", (*arraySection).row_start);
-        pthread_barrier_wait(&barrier1);
+        pthread_barrier_wait(&barrier);
+        pthread_mutex_lock(&(*arraySection).lock1);
+        pthread_mutex_unlock(&(*arraySection).lock1);
 
 
         //iterating array2 and storing it in array1
-        printf("Thread on row %d start iterating\n", (*arraySection).row_start);
         for (int i = (*arraySection).row_start; i < (*arraySection).row_start + (*arraySection).num_rows; i++) {
             for (int j = 1; j < (*arraySection).size; j++) {
                 index = get_array_index(i, j, (*arraySection).size);
                 (*arraySection).array1[index] = average_value((*arraySection).array2, index, (*arraySection).size);
             }
         }
-        printf("Thread on row %d iterated now waiting\n", (*arraySection).row_start);
-        pthread_barrier_wait(&barrier2);
+        pthread_barrier_wait(&barrier);
+        pthread_mutex_lock(&(*arraySection).lock2);
+        pthread_mutex_unlock(&(*arraySection).lock2);
     }
 }
 
@@ -103,7 +104,6 @@ void print_square(double *to_print, int size){
 }
 
 bool within_error(double *array, double *new_array, double error_margin, int size){
-    printf("Checking if Within Error\n");
     int index;
     double difference;
     for(int i = 1; i < size - 1; i++){
@@ -115,7 +115,6 @@ bool within_error(double *array, double *new_array, double error_margin, int siz
             }
         }
     }
-
     return true;
 }
 
@@ -127,9 +126,6 @@ void partition_array(struct doublesArraySection *sections, int num_sections, int
         sections[k].num_rows = rows_per_thread;
     }
     sections[num_sections - 1].num_rows = (array_size - 1) - sections[num_sections - 1].row_start;
-    for(int k = 0; k < num_sections; k++){
-        printf("Section %d starts on row %d and covers %d rows\n", k, sections[k].row_start, sections[k].num_rows);
-    }
 }
 
 void iterate(int num_threads, double *array1, double *array2, int size, double error_margin, bool print_iterations){
@@ -143,28 +139,24 @@ void iterate(int num_threads, double *array1, double *array2, int size, double e
 
     pthread_t *threads = malloc(num_threads*sizeof(pthread_t));
     partition_array(sections, num_threads, size);
-    pthread_barrier_init(&barrier1, NULL, num_threads);
+    pthread_barrier_init(&barrier, NULL, num_threads + 1);
     for(int i = 0; i < num_threads; i++){
         sections[i].array1 = array1;
         sections[i].array2 = array2;
         sections[i].keep_iterating = true;
+        pthread_mutex_init(&sections[i].lock1, NULL);
+        pthread_mutex_init(&sections[i].lock2, NULL);
+        pthread_mutex_lock(&sections[i].lock1);
     }
 
-    printf("Making threads\n");
+    //making threads
     for(int i = 0; i < num_threads; i++){
         pthread_create(&threads[i], NULL, average_section, (void*) &sections[i]);
     }
 
-    printf("Threads made\n");
-
-    //Synchronises Threads before starting
-    pthread_barrier_wait(&barrier0);
-    pthread_barrier_destroy(&barrier0);
     while(keep_iterating){
-        printf("Iterating %d\n", k);
         //iterating array1 and storing it in array2
-        pthread_barrier_wait(&barrier1);
-        printf("all threads hit barrier and done iteration %d\n", k);
+        pthread_barrier_wait(&barrier);
         k++;
 
         if(within_error(array1, array2, error_margin, size)){
@@ -174,35 +166,49 @@ void iterate(int num_threads, double *array1, double *array2, int size, double e
             }
             free(sections);
             printf("%d iterations\n", k);
+            pthread_barrier_destroy(&barrier);
             return;
         }
-        //If not done iterating, destroys barrier and loads new one
-        pthread_barrier_init(&barrier2, NULL, num_threads);
-        pthread_barrier_destroy(&barrier1);
         if(print_iterations) {
             printf("Iteration %d\n", k);
             print_square(array2, size);
         }
+        //If not done iterating, unlocks the threads
+        for(int i = 0; i< num_threads; i++){
+            pthread_mutex_lock(&sections[i].lock2);
+        }
+        for(int i = 0; i < num_threads; i++){
+            pthread_mutex_unlock(&sections[i].lock1);
+        }
 
         //iterating array2 and storing it in array 1
-        pthread_barrier_wait(&barrier2);
+        pthread_barrier_wait(&barrier);
         k++;
         if(within_error(array2, array1, error_margin, size)){
             for(int i = 0; i < num_threads; i++) {
                 sections[i].keep_iterating = false;
+                pthread_mutex_unlock(&sections[i].lock2);
+            }
+            for(int i = 0; i < num_threads; i++){
                 pthread_join(threads[i], NULL);
             }
             free(sections);
             printf("%d iterations\n", k);
+            pthread_barrier_destroy(&barrier);
             return;
         }
-        //If not done iterating, destroys barrier and loads new one
-        pthread_barrier_init(&barrier1, NULL, num_threads);
-        pthread_barrier_destroy(&barrier2);
         if(print_iterations) {
             printf("Iteration %d\n", k);
             print_square(array1, size);
         }
+        //lock
+        for(int i = 0; i< num_threads; i++){
+            pthread_mutex_lock(&sections[i].lock1);
+        }
+        for(int i = 0; i < num_threads; i++){
+            pthread_mutex_unlock(&sections[i].lock2);
+        }
+        printf("Iterating %d\n", k);
     }
     free(sections);
 }
@@ -281,12 +287,12 @@ void generate_tests(int num_threads, int num_tests, int MAX_VALUE, int size, dou
 
 int main(){
     srand(time(NULL));
-    int SIZE = 10;
+    int SIZE = 100;
     int MAX_VALUE = 100;
     int num_test = 1;
     int num_threads = 2;
-    double error_margin = 0.1;
-    bool print_iterations = true;
+    double error_margin = 0.001;
+    bool print_iterations = false;
     bool print_start_end = true;
 
     bool use_random_array = false;
